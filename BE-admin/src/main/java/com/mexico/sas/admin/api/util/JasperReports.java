1b892bd8798b2e5012d9d72707bdbe1503f837be
Oziel Naranjo <oznama27@gmail.com>
2023-10-03 18:33:50 -0600

    Removing JasperReport implementation for kikoya

--- BE-admin/src/main/java/com/mexico/sas/admin/api/util/JasperReports.java
+++ BE-admin/src/main/java/com/mexico/sas/admin/api/util/JasperReports.java
@@ -1,12 +1,9 @@
 package com.mexico.sas.admin.api.util;
 
 import com.mexico.sas.admin.api.constants.GeneralKeys;
-import com.mexico.sas.admin.api.constants.TemplateKeys;
 import com.mexico.sas.admin.api.exception.CustomException;
 import lombok.extern.slf4j.Slf4j;
 import net.sf.jasperreports.engine.*;
-import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
-import net.sf.jasperreports.view.JasperViewer;
 import org.springframework.stereotype.Component;
 
 import java.io.InputStream;
@@ -25,9 +22,7 @@ import java.util.Map;
 public class JasperReports extends Utils {
 
     private final String KEY_REPORT = "/jaspereport/%s.jrxml";
-    private final String CONTACT_ICON = "/jaspereport/Images/contact2.png"; //"/jaspereport/Images/contact.png";
-    private final String DATE_ICON = "/jaspereport/Images/date.png";
-    private final String PDF_PATH = String.format("%s%s", GeneralKeys.TMP_PATH, "/smart-tools/%s_%s.pdf");
+    private final String PDF_PATH = String.format("%s%s", GeneralKeys.TMP_PATH, "/sas/%s_%s.pdf");
 
     public String createPdf(String report, Map<String, Object> parameters, List<?> list) throws CustomException {
         try {
@@ -47,7 +42,6 @@ public class JasperReports extends Utils {
         log.debug("Building report {} to pdf", report);
         try {
             JasperPrint jasperPrint = createJasperPrint(report, parameters, list);
-//            viewReport(jasperPrint);
             JasperExportManager.exportReportToPdfStream(jasperPrint, outputStream);
             log.debug("Report build!");
         } catch (Exception e) {
@@ -62,13 +56,6 @@ public class JasperReports extends Utils {
         InputStream jrmxl = getClass().getResourceAsStream(reportPath);
         try {
             JasperReport jasperReport = JasperCompileManager.compileReport(jrmxl);
-            if(report.equals(TemplateKeys.JR_PROPOSAL_GENERAL)) {
-                log.debug("Adding Bean Collection Data Source with {} elements", list.size());
-                final JRBeanCollectionDataSource source = new JRBeanCollectionDataSource(list);
-                parameters.put("amortizations", source);
-                parameters.put("contactIcon", getClass().getResource(CONTACT_ICON).toString());
-                parameters.put("dateIcon", getClass().getResource(DATE_ICON).toString());
-            }
             return createJasperPrint(jasperReport, parameters);
         } catch (Exception e) {
             log.error("Error creating jasper print!", e);
@@ -80,30 +67,8 @@ public class JasperReports extends Utils {
         log.debug("Creating JasperPrint {} with {} parameters!", jasperReport.getName(), parameters.size());
         try {
             return JasperFillManager.fillReport(jasperReport, parameters, new JREmptyDataSource());
-        } catch (Exception e) {
-            if( e.getMessage().contains("Error opening input stream")) {
-                try {
-                    String urlPathLogo = downloadFile(String.valueOf(parameters.get(TemplateKeys.JR_PARAMETER_LOGO)));
-                    parameters.put(TemplateKeys.JR_PARAMETER_LOGO, urlPathLogo);
-                    JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, new JREmptyDataSource());
-                    deleteFile(urlPathLogo);
-                    return jasperPrint;
-                } catch (JRException e2) {
-                    log.error("Error creating jasper print from JasperReport object!", e);
-                    throw new CustomException(e.getMessage());
-                }
-            } else {
-                log.error("Error creating jasper print from JasperReport object!", e);
-                throw new CustomException(e.getMessage());
-            }
-        }
-    }
-
-    private void viewReport(JasperPrint jasperPrint) {
-        try {
-            JasperViewer.viewReport(jasperPrint);
-        } catch (Exception e) {
-            log.error("Impossible view report, error: {}", e.getMessage());
+        } catch (JRException e) {
+            throw new CustomException(e.getMessage());
         }
     }
 
