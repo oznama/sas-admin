759da66d37c720ddc3b45abb2d2ca316ef59ad56
Oziel Naranjo <oznama27@gmail.com>
2023-12-06 18:24:43 -0600

    Snapshot Frontend 2023-12-06

--- front-admin/src/app/components/Catalogs/CatalogPage.jsx
+++ front-admin/src/app/components/Catalogs/CatalogPage.jsx
@@ -2,19 +2,41 @@ import PropTypes from 'prop-types';
 import { Alert } from '../custom/alerts/page/Alert';
 import { CatalogSingle } from './CatalogSingle';
 import { Catalogs } from './Catalogs';
+import { useState } from 'react';
+import { TableLog } from '../custom/TableLog';
+import { numberToString } from '../../helpers/utils';
+import { useSelector } from 'react-redux';
 
 export const CatalogPage = ({
   title,
   catalogId,
 }) => {
+
+  const { permissions } = useSelector( state => state.auth );
+  const [currentTab, setCurrentTab] = useState(1);
+
+  const renderTabs = () => (
+    <ul className="nav nav-tabs">
+      <li className="nav-item" onClick={ () => setCurrentTab(1) }>
+        <a className={ `nav-link ${ (currentTab === 1) ? 'active' : '' }` }>Detalle</a>
+      </li>
+      <li className="nav-item" onClick={ () => setCurrentTab(2) }>
+        <a className={ `nav-link ${ (currentTab === 2) ? 'active' : '' }` }>Historial</a>
+      </li>
+    </ul>
+  )
+
+  const renderPage = () => catalogId ? <CatalogSingle catalogId={ catalogId } /> : <Catalogs />;
+
   return (
     <>
       <div className='px-5'>
         <div className='d-flex justify-content-between'>
           <h1 className="fs-4 card-title fw-bold mb-4">{ title }</h1>
+          { permissions.isAdminSas && catalogId && renderTabs() }
         </div>
         <Alert />
-        { catalogId ? <CatalogSingle catalogId={ catalogId } /> : <Catalogs /> }
+        { currentTab === 1 ? renderPage() : (<TableLog tableName={ 'Catalog' } recordId={ numberToString(catalogId, '') } />) }
       </div>
     </>
   )
