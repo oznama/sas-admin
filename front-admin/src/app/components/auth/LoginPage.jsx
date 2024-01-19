fa321afd04d551b12ee732399f8f1f4efdf07d92
Oziel Naranjo <oznama27@gmail.com>
2024-01-19 10:52:44 -0600

    FrontEnd SAS logo

--- front-admin/src/app/components/auth/LoginPage.jsx
+++ front-admin/src/app/components/auth/LoginPage.jsx
@@ -7,6 +7,7 @@ import { login } from '../../../store/auth/authSlice';
 import { setMessage } from '../../../store/alert/alertSlice';
 import { doLogin } from '../../services/AuthService';
 import { buildPayloadMessage } from '../../helpers/utils';
+import logo from '../../../assets/img/SAS_logo.png';
 
 export const LoginPage = () => {
 
@@ -37,8 +38,8 @@ export const LoginPage = () => {
             <div className="container h-100">
                 <div className="row justify-content-sm-center h-100">
                     <div className="col-xxl-4 col-xl-5 col-lg-5 col-md-7 col-sm-9">
-                        <div className="text-center my-5">
-                            <img src="https://getbootstrap.com/docs/5.0/assets/brand/bootstrap-logo.svg" alt="logo" width="100" />
+                        <div className="text-center my-2">
+                            <img src={ logo } alt="logo" width="100" />
                         </div>
                         <div className="card shadow-lg">
                             <div className="card-body p-5">
