f076c0a119ce81997aa1574128ed885c8fe48515
Oziel Naranjo <oznama27@gmail.com>
2024-02-06 19:56:33 -0600

    Backend reverse migration v004 and v005

---
+++ BE-admin/src/main/resources/db/migration/V0_0_5__project_alter_add_columns.sql
@@ -0,0 +1,4 @@
+ALTER TABLE project ADD qa_start_date timestamp;
+ALTER TABLE project ADD qa_end_date timestamp;
+ALTER TABLE project ADD delivery_date timestamp;
+ALTER TABLE project ADD follow_code varchar(2);
