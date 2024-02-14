0d82bf72f2dadd515f65ba4e6a1236fea11bc23b
Oziel Naranjo <oznama27@gmail.com>
2024-02-14 00:58:08 -0600

    Frontend fix load project in Detail Project and fix in Detail Application

--- front-admin/src/app/components/projects/ProjectRouter.jsx
+++ front-admin/src/app/components/projects/ProjectRouter.jsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from 'react-router-dom'
-import { ProjectPage } from './ProjectPage'
 import { ApplicationRouter } from '../applications/ApplicationRouter'
 import { OrderRouter } from "../orders/OrderRouter";
+import { DetailProject } from './DetailProject';
 
 export const ProjectRouter = () => (
     <Routes>
-        <Route path="/add" element={ <ProjectPage /> } />
-        <Route path="/:key/edit" element={ <ProjectPage /> } />
+        <Route path="/add" element={ <DetailProject /> } />
+        <Route path="/:key/edit" element={ <DetailProject /> } />
         <Route path="/:projectKey/application/*" element={ <ApplicationRouter /> } />
         <Route path="/:projectKey/order/*" element={ <OrderRouter /> } />
     </Routes>
