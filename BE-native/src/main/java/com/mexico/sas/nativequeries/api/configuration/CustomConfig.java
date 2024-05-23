fcfa3a07456ef9e62d3272bc39647203d8856fd6
Oziel Naranjo <oznama27@gmail.com>
2024-05-23 15:36:25 -0600

    BE-native add cors allowed

--- BE-native/src/main/java/com/mexico/sas/nativequeries/api/configuration/CustomConfig.java
+++ BE-native/src/main/java/com/mexico/sas/nativequeries/api/configuration/CustomConfig.java
@@ -1,12 +1,33 @@
 package com.mexico.sas.nativequeries.api.configuration;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @PropertySources({
         @PropertySource(value = "classpath:/queries/base.properties")
 })
+@Slf4j
 public class CustomConfig {
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        log.debug("Cors configuration source ...");
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setExposedHeaders(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
